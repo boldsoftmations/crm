f1216176b699275385a8a5fc2b7a2095e046b637
vivek-boldSoftMations <vivek@glutape.com>
2023-04-04 13:05:30 +0530

    Updates

---
+++ precache-manifest.9f264fe8d384cacaa618c8f064c11260.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad18976a368fe2ded429",
+    "url": "/crm/static/css/main.7a223f03.chunk.css"
+  },
+  {
+    "revision": "ad18976a368fe2ded429",
+    "url": "/crm/static/js/main.e8c3eba7.chunk.js"
+  },
+  {
+    "revision": "74d3d11ddd56d6450b30",
+    "url": "/crm/static/js/runtime~main.2ce2ed3f.js"
+  },
+  {
+    "revision": "e91aef07fd36ab077b9e",
+    "url": "/crm/static/css/2.1d6953c8.chunk.css"
+  },
+  {
+    "revision": "e91aef07fd36ab077b9e",
+    "url": "/crm/static/js/2.22e9d138.chunk.js"
+  },
+  {
+    "revision": "ba6cbe1188164e4fdba00e0c569bd5ce",
+    "url": "/crm/static/media/LOGOS3.ba6cbe11.png"
+  },
+  {
+    "revision": "92da350d0151010c0b2e7f615099d927",
+    "url": "/crm/static/media/ISOLogo.92da350d.ico"
+  },
+  {
+    "revision": "cc6c45777bc3ce4315ae1fbe24b1fba9",
+    "url": "/crm/static/media/allLogo.cc6c4577.jpg"
+  },
+  {
+    "revision": "185e3428466aee59cf6faf01edf54dfd",
+    "url": "/crm/static/media/MSME.185e3428.jpeg"
+  },
+  {
+    "revision": "411540f229d44e3b54958e6689c46dd4",
+    "url": "/crm/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm/precache-manifest.f2397748453e874d3e2968c7b94ff988.js"
+  "/crm/precache-manifest.9f264fe8d384cacaa618c8f064c11260.js"
 );
 
 workbox.clientsClaim();
