2746f3eb83d97ebcf20f487f2b7e9581f2d1722e
vivek-boldSoftMations <vivek@glutape.com>
2023-04-13 18:34:25 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm/precache-manifest.28d4136afcd50a944e7ed513b9bd59c7.js"
+  "/crm/precache-manifest.a98fd19ec9974200f645ad4751df0e16.js"
 );
 
 workbox.clientsClaim();
