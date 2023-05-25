c6e86e9ba26883ee00e88fbff3109de7b023a0b1
vivek-boldSoftMations <vivek@glutape.com>
2023-05-25 15:57:58 +0530

    fix : in currentforecast change forecast to shortfall and fix pagination of customer not having forecast and dead customer

--- src/Pages/ProductForecast/DeadCustomerView.jsx
+++ src/Pages/ProductForecast/DeadCustomerView.jsx
@@ -161,12 +161,12 @@ export const DeadCustomerView = () => {
       setCurrentPage(page);
       setOpen(true);
 
-      if (searchQuery) {
+      if (searchQuery || filterSelectedQuery) {
         const response =
           await ProductForecastService.getAllDeadCustomerPaginate(
             page,
             filterQuery,
-            searchQuery
+            searchQuery || filterSelectedQuery
           );
         if (response) {
           setDeadCustomer(response.data.results);
