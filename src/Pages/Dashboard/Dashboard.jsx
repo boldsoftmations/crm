b256b1ae7e867d9b8cecb936eb7372d7f5de6c05
vivek-boldSoftMations <vivek@glutape.com>
2023-07-04 13:17:36 +0530

    feat : added date forselect & added usereducer hook  in dashbaord

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import { CustomLoader } from "../../Components/CustomLoader";
 import { OrderBookSummaryView } from "./OrderBookSummary/OrderBookSummaryView";
 import { CurrentSummaryFM } from "./OrderBookSummary/CurrentSummaryFM";
@@ -9,27 +9,24 @@ import InvoiceServices from "../../services/InvoiceService";
 import { SalesPersonSummary } from "./SalesPersonSummary/SalesPersonSummary";
 import { CustomTabs } from "../../Components/CustomTabs";
 import { ProductWiseTurnover } from "./ProductWiseTurnover/ProductWiseTurnover";
-import { Box, Button, Grid, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import { DailyProfitableReports } from "./DailyProfitableReports/DailyProfitableReports";
 import DashboardService from "../../services/DashboardService";
 import { useSelector } from "react-redux";
+import { Popup } from "../../Components/Popup";
 
 export function Dashboard() {
   const [open, setOpen] = useState(false);
-  const [orderBookSummary, setOrderBookSummary] = useState([]);
-  const [currentOrderBookSummaryFM, setCurrentOrderBookSummaryFM] = useState(
-    []
-  );
-  const [currentOrderBookSummaryRM, setCurrentOrderBookSummaryRM] = useState(
-    []
-  );
-  const [currentSalesSummaryFM, setCurrentSalesSummaryFM] = useState([]);
-  const [currentSalesSummaryRM, setCurrentSalesSummaryRM] = useState([]);
-  const [salesPersonSummary, setSalesPersonSummary] = useState([]);
-  const [
-    dailyProfitableReportsFilterData,
-    setDailyProfitableReportsFilterData,
-  ] = useState([]);
+  const [openPopup, setOpenPopup] = useState(false);
   const [activeTab, setActiveTab] = useState(0);
   const [endDate, setEndDate] = useState(new Date());
   const [startDate, setStartDate] = useState(new Date()); // set default value as current date
@@ -37,6 +34,16 @@ export function Dashboard() {
   const maxDate = new Date("2030-12-31").toISOString().split("T")[0];
   const data = useSelector((state) => state.auth);
   const userData = data.profile;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {
+    orderBookSummary,
+    currentOrderBookSummaryFM,
+    currentOrderBookSummaryRM,
+    currentSalesSummaryFM,
+    currentSalesSummaryRM,
+    salesPersonSummary,
+    dailyProfitableReportsFilterData,
+  } = state;
 
   const handleStartDateChange = (event) => {
     const date = new Date(event.target.value);
@@ -47,6 +54,49 @@ export function Dashboard() {
   const handleEndDateChange = (event) => {
     const date = new Date(event.target.value);
     setEndDate(date);
+  };
+
+  const handleChange = (event) => {
+    const selectedValue = event.target.value;
+
+    if (selectedValue === "Today") {
+      const today = new Date();
+      setEndDate(today);
+      setStartDate(today);
+    } else if (selectedValue === "Yesterday") {
+      const yesterday = new Date();
+      yesterday.setDate(yesterday.getDate() - 1);
+      setEndDate(yesterday);
+      setStartDate(yesterday);
+    } else if (selectedValue === "Last 7 Days") {
+      const endDate = new Date();
+      const startDate = new Date();
+      startDate.setDate(startDate.getDate() - 7);
+      setEndDate(endDate);
+      setStartDate(startDate);
+    } else if (selectedValue === "Last 30 Days") {
+      const endDate = new Date();
+      const startDate = new Date();
+      startDate.setDate(startDate.getDate() - 30);
+      setEndDate(endDate);
+      setStartDate(startDate);
+    } else if (selectedValue === "This Month") {
+      const endDate = new Date();
+      const startDate = new Date(endDate.getFullYear(), endDate.getMonth(), 1);
+      setEndDate(endDate);
+      setStartDate(startDate);
+    } else if (selectedValue === "Last Month") {
+      const endDate = new Date();
+      endDate.setDate(0);
+      const startDate = new Date(endDate.getFullYear(), endDate.getMonth(), 1);
+      setEndDate(endDate);
+      setStartDate(startDate);
+    } else if (selectedValue === "Custom Date") {
+      // Handle custom date logic, for example:
+      setStartDate(new Date());
+      setEndDate(new Date());
+      setOpenPopup(true);
+    }
   };
 
   const getResetData = () => {
@@ -92,11 +142,26 @@ export function Dashboard() {
         StartDate,
         EndDate
       );
-      setOrderBookSummary(response.data.Order_Book_Summary);
-      setCurrentOrderBookSummaryFM(response.data.Order_Book_FG);
-      setCurrentOrderBookSummaryRM(response.data.Order_Book_RM);
-      setCurrentSalesSummaryFM(response.data.Sales_Invoice_FG);
-      setCurrentSalesSummaryRM(response.data.Sales_Invoice_RM);
+      dispatch({
+        type: "SET_ORDER_BOOK_SUMMARY",
+        payload: response.data.Order_Book_Summary,
+      });
+      dispatch({
+        type: "SET_CURRENT_ORDER_BOOK_SUMMARY_FM",
+        payload: response.data.Order_Book_FG,
+      });
+      dispatch({
+        type: "SET_CURRENT_ORDER_BOOK_SUMMARY_RM",
+        payload: response.data.Order_Book_RM,
+      });
+      dispatch({
+        type: "SET_CURRENT_SALES_SUMMARY_FM",
+        payload: response.data.Sales_Invoice_FG,
+      });
+      dispatch({
+        type: "SET_CURRENT_SALES_SUMMARY_RM",
+        payload: response.data.Sales_Invoice_RM,
+      });
       // setSalesPersonSummary(response.data.sales_summary);
       setOpen(false);
     } catch (err) {
@@ -115,7 +180,10 @@ export function Dashboard() {
           StartDate,
           EndDate
         );
-      setDailyProfitableReportsFilterData(response.data);
+      dispatch({
+        type: "SET_DAILY_PROFITABLE_REPORTS_FILTER_DATA",
+        payload: response.data,
+      });
       setOpen(false);
     } catch (err) {
       setOpen(false);
@@ -127,16 +195,34 @@ export function Dashboard() {
     try {
       setOpen(true);
       const response = await InvoiceServices.getAllDashboardData();
-      setOrderBookSummary(response.data.Order_Book_Summary);
-      setCurrentOrderBookSummaryFM(response.data.Order_Book_FG);
-      setCurrentOrderBookSummaryRM(response.data.Order_Book_RM);
-      setCurrentSalesSummaryFM(response.data.Sales_Invoice_FG);
-      setCurrentSalesSummaryRM(response.data.Sales_Invoice_RM);
-      setSalesPersonSummary(response.data.sales_summary);
+      dispatch({
+        type: "SET_ORDER_BOOK_SUMMARY",
+        payload: response.data.Order_Book_Summary,
+      });
+      dispatch({
+        type: "SET_CURRENT_ORDER_BOOK_SUMMARY_FM",
+        payload: response.data.Order_Book_FG,
+      });
+      dispatch({
+        type: "SET_CURRENT_ORDER_BOOK_SUMMARY_RM",
+        payload: response.data.Order_Book_RM,
+      });
+      dispatch({
+        type: "SET_CURRENT_SALES_SUMMARY_FM",
+        payload: response.data.Sales_Invoice_FG,
+      });
+      dispatch({
+        type: "SET_CURRENT_SALES_SUMMARY_RM",
+        payload: response.data.Sales_Invoice_RM,
+      });
+      dispatch({
+        type: "SET_SALES_PERSON_SUMMARY",
+        payload: response.data.sales_summary,
+      });
       setOpen(false);
     } catch (err) {
       setOpen(false);
-      console.log("company data by id error", err);
+      console.log("Dashboard error", err);
     }
   };
 
@@ -160,44 +246,21 @@ export function Dashboard() {
         >
           <Grid container spacing={2}>
             <Grid item xs={5} sm={5} md={5} lg={5}>
-              <TextField
-                fullWidth
-                label="Start Date"
-                variant="outlined"
-                size="small"
-                type="date"
-                id="start-date"
-                value={startDate ? startDate.toISOString().split("T")[0] : ""}
-                min={minDate}
-                max={maxDate}
-                onChange={handleStartDateChange}
-              />
-            </Grid>
-            <Grid item xs={5} sm={5} md={5} lg={5}>
-              <TextField
-                fullWidth
-                label="End Date"
-                variant="outlined"
-                size="small"
-                type="date"
-                id="end-date"
-                value={endDate ? endDate.toISOString().split("T")[0] : ""}
-                min={
-                  startDate ? startDate.toISOString().split("T")[0] : minDate
-                }
-                max={maxDate}
-                onChange={handleEndDateChange}
-                disabled={!startDate}
-              />
-            </Grid>
-            <Grid item xs={2} sm={2} md={2} lg={2}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={getResetData}
-              >
-                Reset
-              </Button>
+              <FormControl fullWidth size="small">
+                <InputLabel id="demo-select-small">Date</InputLabel>
+                <Select
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  label="Date"
+                  onChange={(event) => handleChange(event)}
+                >
+                  {DateOptions.map((option, i) => (
+                    <MenuItem key={i} value={option.value}>
+                      {option.value}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
             </Grid>
           </Grid>
         </Box>
@@ -253,6 +316,122 @@ export function Dashboard() {
           )}
         </div>
       </div>
+      <Popup
+        openPopup={openPopup}
+        setOpenPopup={setOpenPopup}
+        title="Date Filter"
+        maxWidth="md"
+      >
+        <Box
+          sx={{
+            backgroundColor: "white",
+            borderRadius: "8px",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+            margin: "10px",
+            padding: "20px",
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={5} sm={5} md={5} lg={5}>
+              <TextField
+                fullWidth
+                label="Start Date"
+                variant="outlined"
+                size="small"
+                type="date"
+                id="start-date"
+                value={startDate ? startDate.toISOString().split("T")[0] : ""}
+                min={minDate}
+                max={maxDate}
+                onChange={handleStartDateChange}
+              />
+            </Grid>
+            <Grid item xs={5} sm={5} md={5} lg={5}>
+              <TextField
+                fullWidth
+                label="End Date"
+                variant="outlined"
+                size="small"
+                type="date"
+                id="end-date"
+                value={endDate ? endDate.toISOString().split("T")[0] : ""}
+                min={
+                  startDate ? startDate.toISOString().split("T")[0] : minDate
+                }
+                max={maxDate}
+                onChange={handleEndDateChange}
+                disabled={!startDate}
+              />
+            </Grid>
+            <Grid item xs={2} sm={2} md={2} lg={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={getResetData}
+              >
+                Reset
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Popup>
     </div>
   );
 }
+
+const DateOptions = [
+  {
+    value: "Today",
+  },
+  {
+    value: "Yesterday",
+  },
+  {
+    value: "Last 7 Days",
+  },
+  {
+    value: "Last 30 Days",
+  },
+  {
+    value: "This Month",
+  },
+  {
+    value: "Last Month",
+  },
+  {
+    value: "Custom Date",
+  },
+];
+
+// Define initial state for the reducer
+const initialState = {
+  orderBookSummary: [],
+  currentOrderBookSummaryFM: [],
+  currentOrderBookSummaryRM: [],
+  currentSalesSummaryFM: [],
+  currentSalesSummaryRM: [],
+  salesPersonSummary: [],
+  dailyProfitableReportsFilterData: [],
+};
+
+// Define the reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_ORDER_BOOK_SUMMARY":
+      return { ...state, orderBookSummary: action.payload };
+    case "SET_CURRENT_ORDER_BOOK_SUMMARY_FM":
+      return { ...state, currentOrderBookSummaryFM: action.payload };
+    case "SET_CURRENT_ORDER_BOOK_SUMMARY_RM":
+      return { ...state, currentOrderBookSummaryRM: action.payload };
+    case "SET_CURRENT_SALES_SUMMARY_FM":
+      return { ...state, currentSalesSummaryFM: action.payload };
+    case "SET_CURRENT_SALES_SUMMARY_RM":
+      return { ...state, currentSalesSummaryRM: action.payload };
+    case "SET_SALES_PERSON_SUMMARY":
+      return { ...state, salesPersonSummary: action.payload };
+    case "SET_DAILY_PROFITABLE_REPORTS_FILTER_DATA":
+      return { ...state, dailyProfitableReportsFilterData: action.payload };
+    default:
+      return state;
+  }
+};
