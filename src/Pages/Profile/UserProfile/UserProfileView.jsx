354b4931f44e133f026354dbe59f850e0a6f9dbe
boldsoftmations <vivek@glutape.com>
2023-10-27 18:26:23 +0530

    feat : added download csv in userprofileview

--- src/Pages/Profile/UserProfile/UserProfileView.jsx
+++ src/Pages/Profile/UserProfile/UserProfileView.jsx
@@ -5,6 +5,7 @@ import { CustomTable } from "../../../Components/CustomTable";
 import { CustomSearch } from "../../../Components/CustomSearch";
 import { Popup } from "../../../Components/Popup";
 import { UserProfileUpdate } from "./UserProfileUpdate";
+import { CSVLink } from "react-csv";
 
 export const UserProfileView = () => {
   const [openPopup, setOpenPopup] = useState(false);
@@ -46,18 +47,211 @@ export const UserProfileView = () => {
     "ACTION",
   ];
 
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const options = { day: "2-digit", month: "2-digit", year: "numeric" };
-    return new Intl.DateTimeFormat("en-GB", options).format(date);
-  };
-
   const filteredUserProfiles = userProfiles.filter((user) =>
     Object.values(user.personal || {}).some((value) =>
       String(value).toLowerCase().includes(searchQuery.toLowerCase())
     )
   );
 
+  const formatDate = (dateString) => {
+    return dateString
+      ? new Intl.DateTimeFormat("en-GB", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        }).format(new Date(dateString))
+      : "-";
+  };
+
+  const getCsvData = () => {
+    return filteredUserProfiles.map((user) => {
+      const personal = user.personal || {};
+      const addressCurrent = user.address.current || {};
+      const addressPermanent = user.address.permanent || {};
+      const kyc = user.kyc || {};
+      const pfEsiDetails = user.pf_esi_details || {};
+      const employmentHistory = user.employment_history || [{}];
+      const education = user.education || {};
+      const emergencyContacts = user.emergency_contacts || [{}];
+      const familyDetails = user.family_details || [{}];
+      const medical = user.medical || {};
+      const addiction = user.addiction || {};
+      const doctor = user.docter || {};
+      // Check if education.diploma exists before accessing its properties
+      const school = education.school || {};
+      const college = education.college || {};
+      const diploma = education.diploma || {};
+      const graduation = education.graduation || {};
+      const pg = education.pg || {};
+      const additional_qualification = education.additional_qualifiction || {};
+
+      return {
+        user: personal.user || "-",
+        first_name: personal.first_name || "-",
+        middle_name: personal.middle_name || "-",
+        last_name: personal.last_name || "-",
+        email: personal.email || "-",
+        contact: personal.contact || "-",
+        date_of_birth: personal.date_of_birth || "-",
+        place_of_birth: personal.place_of_birth || "-",
+        nationality: personal.nationality || "-",
+        marital_status: personal.marital_status || "-",
+        marriage_date: personal.marriage_date || "-",
+        date_of_joining: personal.date_of_joining || "-",
+        blood_group: personal.blood_group || "-",
+
+        // Current Address
+        current_address: addressCurrent.address || "-",
+        current_city: addressCurrent.city || "-",
+        current_state: addressCurrent.state || "-",
+        current_pin: addressCurrent.pin || "-",
+
+        // Permanent Address
+        permanent_address: addressPermanent.address || "-",
+        permanent_city: addressPermanent.city || "-",
+        permanent_state: addressPermanent.state || "-",
+        permanent_pin: addressPermanent.pin || "-",
+
+        // KYC
+        kyc_name: kyc.name || "-",
+        account_number: kyc.account_number || "-",
+        ifsc_code: kyc.ifsc_code || "-",
+        branch: kyc.branch || "-",
+        kyc_city: kyc.city || "-",
+        kyc_state: kyc.state || "-",
+        kyc_address: kyc.address || "-",
+        pan_card_number: kyc.pan_card_number || "-",
+        aadhar_card_number: kyc.aadhar_card_number || "-",
+        passport_number: kyc.passport_number || "-",
+        dl_number: kyc.dl_number || "-",
+
+        // Emergency Contacts
+        emergency_contact_name: emergencyContacts[0].name || "-",
+        emergency_contact_relationship:
+          emergencyContacts[0].relationship || "-",
+        emergency_contact_number: emergencyContacts[0].number || "-",
+
+        // PF/ESI Details
+        has_pf_esi_account: pfEsiDetails.has_pf_esi_account || "-",
+        uan_number: pfEsiDetails.uan_number || "-",
+        pf_number: pfEsiDetails.pf_number || "-",
+        esi_number: pfEsiDetails.esi_number || "-",
+
+        // Employment History
+        employment_company_name: employmentHistory[0].company_name || "-",
+        employment_post_held: employmentHistory[0].post_held || "-",
+        employment_workedFrom: employmentHistory[0].workedFrom || "-",
+        employment_workedTill: employmentHistory[0].workedTill || "-",
+
+        // Education
+        school_name: school.name || "-",
+        school_board: school.board || "-",
+        school_passout: school.passout || "-",
+        college_name: college.name || "-",
+        college_board: college.board || "-",
+        college_passout: college.passout || "-",
+        diploma_type: diploma.type || "-",
+        diploma_uni_name: diploma.uni_name || "-",
+        diploma_passout: diploma.passout || "-",
+        graduation_type: graduation.type || "-",
+        graduation_university: graduation.university || "-",
+        graduation_passout: graduation.passout || "-",
+        pg_masters: pg.masters || "-",
+        pg_passout: pg.passout || "-",
+        additional_qualification: additional_qualification || "-",
+
+        // Medical
+        surgery_type: medical.surgery_type || "-",
+        pregnancy: medical.pregnancy || "-",
+        previous_surgeries: medical.previous_surgeries || "-",
+        known_allergies: medical.known_allergies || "-",
+        diabetic: medical.diabetic || "-",
+        hyper_tension: medical.hyper_tension || "-",
+        heart_issues: medical.heart_issues || "-",
+        cancer: medical.cancer || "-",
+        high_blood_pressure: medical.high_blood_pressure || "-",
+        low_blood_pressure: medical.low_blood_pressure || "-",
+        asthama_respiratory: medical.asthama_respiratory || "-",
+        vision: medical.vision || "-",
+        hearing: medical.hearing || "-",
+
+        // Addiction
+        tobacco: addiction.tobacco || "-",
+        cigarettes: addiction.cigarettes || "-",
+        alcohol: addiction.alcohol || "-",
+
+        // Doctor
+        doctor_name: doctor.name || "-",
+        doctor_phone_number: doctor.phone_number || "-",
+
+        // Family Details
+        family_member_name: familyDetails[0].name || "-",
+        family_member_relationship: familyDetails[0].relationship || "-",
+        family_member_blood_group: familyDetails[0].blood_group || "-",
+        family_member_contact_number: familyDetails[0].contact_number || "-",
+      };
+    });
+  };
+
+  const getCsvHeaders = () => {
+    const user = filteredUserProfiles[0]; // Assuming there's at least one user in the array
+    const headers = [];
+
+    if (user) {
+      for (const key in user) {
+        if (user.hasOwnProperty(key) && typeof user[key] !== "object") {
+          // Exclude nested objects (e.g., personal, address, etc.)
+          headers.push(key);
+        }
+      }
+
+      // Add headers for education properties
+      const educationKeys = [
+        "school_name",
+        "school_board",
+        "school_passout",
+        "college_name",
+        "college_board",
+        "college_passout",
+        "diploma_type",
+        "diploma_uni_name",
+        "diploma_passout",
+        "graduation_type",
+        "graduation_university",
+        "graduation_passout",
+        "pg_masters",
+        "pg_passout",
+        "additional_qualification",
+      ];
+
+      headers.push(...educationKeys);
+
+      // Add headers for medical properties
+      const medicalKeys = [
+        "surgery_type",
+        "pregnancy",
+        "previous_surgeries",
+        "known_allergies",
+        "diabetic",
+        "hyper_tension",
+        "heart_issues",
+        "cancer",
+        "high_blood_pressure",
+        "low_blood_pressure",
+        "asthama_respiratory",
+        "vision",
+        "hearing",
+      ];
+
+      headers.push(...medicalKeys);
+    }
+
+    return headers;
+  };
+
+  const csvHeaders = getCsvHeaders();
+
+  const csvData = getCsvData();
   const data = filteredUserProfiles.map((user) => ({
     id: user.id,
     first_name: user.personal.first_name || "-",
@@ -92,6 +286,15 @@ export const UserProfileView = () => {
           </div>
           <div style={{ flexGrow: 2 }}>
             <h3 style={styles.header}>User Profile</h3>
+          </div>
+          <div style={{ flexGrow: 1 }}>
+            <CSVLink
+              data={csvData}
+              headers={csvHeaders}
+              filename={"user_profiles.csv"}
+            >
+              Download CSV
+            </CSVLink>
           </div>
         </div>
         {filteredUserProfiles.length > 0 ? (
