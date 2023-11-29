f58af31d5bfefa73ecfd475447bbc0fadae063de
BSrushil <rushil@boldsoftmation.com>
2023-11-29 17:29:18 +0530

    Feat: Mis report

--- src/Pages/HrModel/MisReport/MisReportView.jsx
+++ src/Pages/HrModel/MisReport/MisReportView.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import { Container, Typography, Grid } from "@mui/material";
+import { Container, Typography, Grid, Box } from "@mui/material";
 import { CustomChart } from "../../../Components/CustomChart";
+import Chart from "react-google-charts";
 
 export const MisReportView = () => {
-  const positions = [
-    { title: "Software Developer", candidates: 12, source: "Naukri" },
-    { title: "Backend Developer", candidates: 18, source: "JobHai" },
-    { title: "Devops Engineer", candidates: 3, source: "LinkdIn" },
+  const jobOpeningData = [
+    ["Job Category", "Count"],
+    ["Total Job Openings", 21],
+    ["Open Job Count", 3],
+    ["New Job Openings", 2],
   ];
 
   const pipelineData = [
     ["Stage", "Number"],
-    ["Called", 30],
-    ["Selected for Interview", 15],
-    ["Selected for Offer", 10],
-    ["Offered", 8],
-    ["Joined", 5],
+    ["Total Applicants", 14],
+    ["Shortlisted Applicants", 9],
   ];
 
-  const positionsChartData = [
-    ["Position", "Candidates"],
-    ...positions.map((position) => [position.title, position.candidates]),
+  const applicationSourcesData = [
+    ["Source", "Total"],
+    ["GlassDoor", 1],
+    ["Indeed", 2],
+    ["JobHai", 2],
+    ["Naukri", 7],
   ];
 
   return (
@@ -29,36 +31,55 @@ export const MisReportView = () => {
         MIS Report
       </Typography>
       <Grid container spacing={3}>
-        {/* Pipeline Chart */}
-        <Grid item xs={12}>
-          <CustomChart
-            chartType="PieChart"
-            data={pipelineData}
-            options={{
-              title: "Pipeline Status",
-              hAxis: { title: "Stage" },
-              vAxis: { title: "Number of Candidates" },
-              legend: "none",
-            }}
-            widthStyle="100%"
-            heightStyle="400px"
-          />
+        {/* Pipeline Data Chart */}
+        <Grid item xs={12} md={6}>
+          <Box boxShadow={3}>
+            <CustomChart
+              chartType="PieChart"
+              data={pipelineData}
+              options={{
+                title: "Recruitment Pipeline",
+                hAxis: { title: "Stage" },
+                vAxis: { title: "Number" },
+                legend: "none",
+              }}
+              widthStyle="100%"
+              heightStyle="400px"
+            />
+          </Box>
         </Grid>
 
-        {/* Positions Chart */}
+        {/* Job Opening Data Chart */}
+        <Grid item xs={12} md={6}>
+          <Box boxShadow={3}>
+            <Chart
+              chartType="PieChart"
+              data={jobOpeningData}
+              options={{
+                title: "Job Opening Details",
+                pieHole: 0.4,
+              }}
+              width="100%"
+              height="400px"
+            />
+          </Box>
+        </Grid>
+        {/* Application Sources Chart */}
         <Grid item xs={12}>
-          <CustomChart
-            chartType="BarChart"
-            data={positionsChartData}
-            options={{
-              title: "Candidates Shortlisted for Positions",
-              hAxis: { title: "Position" },
-              vAxis: { title: "Number of Candidates" },
-              legend: "none",
-            }}
-            widthStyle="100%"
-            heightStyle="400px"
-          />
+          <Box boxShadow={3}>
+            <CustomChart
+              chartType="BarChart"
+              data={applicationSourcesData}
+              options={{
+                title: "Application Sources",
+                vAxis: { title: "Source" },
+                hAxis: { title: "Total Applications" },
+                legend: "none",
+              }}
+              widthStyle="100%"
+              heightStyle="400px"
+            />
+          </Box>
         </Grid>
       </Grid>
     </Container>
