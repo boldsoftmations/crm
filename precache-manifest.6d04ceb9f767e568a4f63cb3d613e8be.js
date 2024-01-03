6b5ccbe5752afa0821f9952b9221d65270325ac1
BSrushil <rushil@boldsoftmation.com>
2024-01-03 18:16:26 +0530

    Updates

---
+++ precache-manifest.6d04ceb9f767e568a4f63cb3d613e8be.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "647e8d0ff13d786f82d7",
+    "url": "/crm/static/css/main.15a016fc.chunk.css"
+  },
+  {
+    "revision": "647e8d0ff13d786f82d7",
+    "url": "/crm/static/js/main.9cd072a8.chunk.js"
+  },
+  {
+    "revision": "c36c286fc7c0daa240f4",
+    "url": "/crm/static/js/runtime~main.3f9a4389.js"
+  },
+  {
+    "revision": "6c0d2c2a9a87e22bd9a8",
+    "url": "/crm/static/css/2.a49d2112.chunk.css"
+  },
+  {
+    "revision": "6c0d2c2a9a87e22bd9a8",
+    "url": "/crm/static/js/2.c49c5ab8.chunk.js"
+  },
+  {
+    "revision": "69464aa103244568635c",
+    "url": "/crm/static/js/3.d3a461fe.chunk.js"
+  },
+  {
+    "revision": "d7be75014b6ee7274355",
+    "url": "/crm/static/js/4.0c250613.chunk.js"
+  },
+  {
+    "revision": "dfedc4bcffac31bf2b11",
+    "url": "/crm/static/js/5.201444cd.chunk.js"
+  },
+  {
+    "revision": "ba6cbe1188164e4fdba00e0c569bd5ce",
+    "url": "/crm/static/media/LOGOS3.ba6cbe11.png"
+  },
+  {
+    "revision": "cc6c45777bc3ce4315ae1fbe24b1fba9",
+    "url": "/crm/static/media/allLogo.cc6c4577.jpg"
+  },
+  {
+    "revision": "185e3428466aee59cf6faf01edf54dfd",
+    "url": "/crm/static/media/MSME.185e3428.jpeg"
+  },
+  {
+    "revision": "92da350d0151010c0b2e7f615099d927",
+    "url": "/crm/static/media/ISOLogo.92da350d.ico"
+  },
+  {
+    "revision": "293088e28ee37ad39c6c0572c29286e6",
+    "url": "/crm/static/media/ISO.293088e2.png"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/crm/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "66209ae01f484e46679622dd607fcbc5",
+    "url": "/crm/static/media/Roboto-Thin.66209ae0.ttf"
+  },
+  {
+    "revision": "8a36205bd9b83e03af0591a004bc97f4",
+    "url": "/crm/static/media/Roboto-Regular.8a36205b.ttf"
+  },
+  {
+    "revision": "68ea4734cf86bd544650aee05137d7bb",
+    "url": "/crm/static/media/Roboto-Medium.68ea4734.ttf"
+  },
+  {
+    "revision": "881e150ab929e26d1f812c4342c15a7c",
+    "url": "/crm/static/media/Roboto-Light.881e150a.ttf"
+  },
+  {
+    "revision": "b8e42971dec8d49207a8c8e2b919a6ac",
+    "url": "/crm/static/media/Roboto-Bold.b8e42971.ttf"
+  },
+  {
+    "revision": "b82bf94b304a59cf5d9550dfcbd5ef6d",
+    "url": "/crm/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm/precache-manifest.d341e0810f3522ca97241e29da39df60.js"
+  "/crm/precache-manifest.6d04ceb9f767e568a4f63cb3d613e8be.js"
 );
 
 workbox.clientsClaim();
