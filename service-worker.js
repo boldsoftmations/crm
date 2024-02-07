28a0a48295628f50fc07200f68440b41a99c176f
BSrushil <rushil@boldsoftmation.com>
2024-02-07 12:59:27 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm/precache-manifest.41d41497ff5bacc62f9992e97a118be1.js"
+  "/crm/precache-manifest.eeee135406a744a9e1785794c24249a7.js"
 );
 
 workbox.clientsClaim();
