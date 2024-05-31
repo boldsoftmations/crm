b5ef43f2d10443fbbc3ef3704bf826518c441361
adityaJaiswar24 <aditya@boldsoftmation.com>
2024-05-31 12:11:32 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm/precache-manifest.14eb829c2a456730bcf48d7831369e25.js"
+  "/crm/precache-manifest.29fb77838219a95b25a3675f6a7589d7.js"
 );
 
 workbox.clientsClaim();
