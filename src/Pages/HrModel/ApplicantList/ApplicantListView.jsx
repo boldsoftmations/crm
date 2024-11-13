631bfdaf42a958a073f29a190072c0147cdc383e
boldsoftmations <vivek@glutape.com>
2024-11-13 11:28:23 +0530

    update in jd and Applicants view

--- src/Pages/HrModel/ApplicantList/ApplicantListView.jsx
+++ src/Pages/HrModel/ApplicantList/ApplicantListView.jsx
@@ -178,7 +178,7 @@ export const ApplicantListView = () => {
         fetchApplicants();
       }
     } catch (error) {
-      alertmsg({
+      setAlertMsg({
         message:
           (error && error.response.data.message) ||
           "Error sending Whatsapp Message",
@@ -212,7 +212,7 @@ export const ApplicantListView = () => {
         fetchApplicants();
       }
     } catch (error) {
-      alertmsg({
+      setAlertMsg({
         message:
           (error && error.response.data.message) ||
           "Error sending Email Message",
