049266aeeabdf2843c5b2e5b3e3c6715fa7b7ad4
Aditya <vivek@glutape.com>
2025-07-21 17:09:49 +0530

    complaint Filter and Ui change

--- src/Pages/CCF/AllComplaintListView.jsx
+++ src/Pages/CCF/AllComplaintListView.jsx
@@ -78,16 +78,6 @@ export const AllComplaintListView = () => {
     getAllComplaintsList();
   }, [currentPage, searchQuery]);
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-    setCurrentPage(1); // Reset to first page with new search
-  };
-
-  const handleReset = () => {
-    setSearchQuery("");
-    setCurrentPage(1); // Reset to first page with no search query
-  };
-
   const handlePageChange = (event, value) => {
     setCurrentPage(value);
   };
@@ -109,33 +99,8 @@ export const AllComplaintListView = () => {
             }}
           >
             <Grid container spacing={2} alignItems="center">
-              {/* Search Component on the left */}
-              <Grid item xs={12} md={3}>
-                <SearchComponent
-                  onSearch={handleSearch}
-                  onReset={handleReset}
-                />
-              </Grid>
-
               {/* Title Text centered */}
-              <Grid
-                item
-                xs={12}
-                md={4}
-                sx={{ textAlign: { xs: "center", md: "center" } }}
-              >
-                <h3
-                  style={{
-                    margin: 0,
-                    fontSize: "24px",
-                    color: "rgb(34, 34, 34)",
-                    fontWeight: 800,
-                  }}
-                >
-                  Complaints Choice
-                </h3>
-              </Grid>
-              <Grid item xs={12} md={3}>
+              <Grid item xs={12} md={4}>
                 <CustomAutocomplete
                   fullWidth
                   size="small"
@@ -155,13 +120,24 @@ export const AllComplaintListView = () => {
               <Grid
                 item
                 xs={12}
-                md={2}
-                sx={{
-                  display: "flex",
-                  justifyContent: { xs: "center", md: "flex-end" },
-                }}
+                md={4}
+                sx={{ textAlign: { xs: "center", md: "center" } }}
               >
+                <h3
+                  style={{
+                    margin: 0,
+                    fontSize: "24px",
+                    color: "rgb(34, 34, 34)",
+                    fontWeight: 800,
+                  }}
+                >
+                  Complaints Choice
+                </h3>
+              </Grid>
+
+              <Grid item xs={12} md={4} sx={{ textAlign: "end" }}>
                 <Button
+                  sx={{ textAlign: "right" }}
                   color="primary"
                   variant="contained"
                   onClick={() => setOpenAddComplainttype(true)}
